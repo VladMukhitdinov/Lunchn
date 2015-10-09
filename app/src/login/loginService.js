657098403f4680d05b6e3984e1345f0e1204f83a
Vlad Mukhitdinov <Vlad.Mukhitdinov@lic.co.nz>
2015-10-09 13:58:13 +1300

    got rid off google login page

--- app/src/login/loginService.js
+++ app/src/login/loginService.js
@@ -11,14 +11,8 @@
     return {
       attemptSignIn : function(provider) {
         // Simulate async nature of real remote calls
-		$auth.authenticate(provider) 
-		.then(function(response) {
-			alert('Signed in with Google+');
-		})
-		.catch(function(response) {
-			alert('Error ocurred' + response);
-		})
-		
+
+
         return $q.when(user);
       }
     };
